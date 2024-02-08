c04b33307246c6b303b89197b591ca0569b8145d
Shitcoder731 <shitanshu.731@gmail.com>
2024-02-08 10:32:36 +0530

    Added Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react'
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import {lazy} from "react"
 import Loader from './components/Loader'
+import Header from './components/Header'
 
 const Home = lazy(() => import("./pages/Home"))
 const Search = lazy(() => import("./pages/Search"))
@@ -33,6 +34,7 @@ const App = () => {
   return (
     <Router>
       {/* Header */}
+      <Header />
       <Suspense fallback = {<Loader />}>
       <Routes>
         <Route path = "/" element ={<Home />} /> 
