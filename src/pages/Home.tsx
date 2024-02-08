0b8a357a4facfc1244c0578423ef4898d1e4fce6
Shitcoder731 <shitanshu.731@gmail.com>
2024-02-08 19:19:11 +0530

    Added ProductCard Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,11 @@
 import { Link } from "react-router-dom";
+import ProductCard from "../components/Product-card";
 
 
 const Home = () => {
-  // const { data, isLoading, isError } = useLatestProductsQuery("");
 
 
-  // const addToCartHandler = (cartItem: CartItem) => {
-  //   if (cartItem.stock < 1) return toast.error("Out of Stock");
-  //   dispatch(addToCart(cartItem));
-  //   toast.success("Added to cart");
-  // };
-
-  // if (isError) toast.error("Cannot Fetch the Products");
+  const addToCartHandler = () => {};
 
   return (
     <div className="home">
@@ -25,21 +19,33 @@ const Home = () => {
       </h1>
 
       <main>
-        {/* {isLoading ? (
-          <Skeleton width="80vw" />
-        ) : (
-          data?.products.map((i) => (
             <ProductCard
-              key={i._id}
-              productId={i._id}
-              name={i.name}
-              price={i.price}
-              stock={i.stock}
+              key="asdads"
+              productId={124335}
+              name="Asus 11"
+              price={130000}
+              stock={12}
               handler={addToCartHandler}
-              photo={i.photo}
+              photo="https://s.yimg.com/os/creatr-uploaded-images/2020-12/d37e0460-3eeb-11eb-b7f9-449e864b9135"
             />
-          ))
-        )} */}
+            <ProductCard
+              key="asdads"
+              productId={124335}
+              name="Iphone 15 pro"
+              price={120000}
+              stock={12}
+              handler={addToCartHandler}
+              photo="https://www.91-cdn.com/pricebaba-blogimages/wp-content/uploads/2022/12/iPhone-15-Ultra.jpg"
+            />
+            <ProductCard
+              key="asdads"
+              productId={124335}
+              name="PS 5"
+              price={72000}
+              stock={12}
+              handler={addToCartHandler}
+              photo="https://s.yimg.com/os/creatr-uploaded-images/2020-11/7c9dc7a0-24f3-11eb-aed4-9f1ba3e2dec3"
+            />
       </main>
     </div>
   );
