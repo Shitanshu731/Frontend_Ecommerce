8c2e66b4a6a62a7ea703a8005a478d45bd842404
Shitcoder731 <shitanshu.731@gmail.com>
2024-02-08 08:02:19 +0530

    Added Loader For each Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import {lazy} from "react"
+import Loader from './components/Loader'
 
 const Home = lazy(() => import("./pages/Home"))
 const Search = lazy(() => import("./pages/Search"))
@@ -9,11 +10,13 @@ const App = () => {
   return (
     <Router>
       {/* Header */}
+      <Suspense fallback = {<Loader />}>
       <Routes>
         <Route path = "/" element ={<Home />} /> 
         <Route path = "/search" element ={<Search />} />
         <Route path = "/cart" element ={<Cart />} />
       </Routes>
+      </Suspense>
     </Router>
   )
 }
