f6baaea99c1a72c75ddbd7e6cddfc97527e224f2
Shitcoder731 <shitanshu.731@gmail.com>
2024-02-08 07:46:52 +0530

    Created Fromtend for the Ecommerce

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Cart from './pages/Cart'
+
+const App = () => {
+  return (
+    <Router>
+      {/* Header */}
+      <Routes>
+        <Route path = "/" element ={<Home />} /> 
+        <Route path = "/search" element ={<Search />} />
+        <Route path = "/cart" element ={<Cart />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
