9f4cb138481e3b8dae3d0d1bebbb6f28d33efe24
Shitcoder731 <shitanshu.731@gmail.com>
2024-02-11 12:54:16 +0530

    Added Search Page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,11 +1,77 @@
-import React from 'react'
-
+import { useState } from "react";
+import ProductCard from "../components/Product-card";
 const Search = () => {
-  return (
-    <div>
-      Search
-    </div>
-  )
-}
 
-export default Search
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [maxPrice, setMaxPrice] = useState(100000);
+  const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
+  const [productLoading, setproductLoading] = useState(false);
+  const addToCartHandler = () => {}
+  return (
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="">None</option>
+            <option value="asc">Price (Low to High)</option>
+            <option value="dsc">Price (High to Low)</option>
+          </select>
+        </div>
+
+        <div>
+          <h4>Max Price: {maxPrice || ""}</h4>
+          <input
+            type="range"
+            min={100}
+            max={100000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          />
+        </div>
+
+        <div>
+          <h4>Category</h4>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">ALL</option>
+          </select>
+        </div>
+      </aside>
+      <main>
+        <h1>Products</h1>
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        {productLoading ? (
+          "<Skeleton length={10} />"
+        ) : (
+          <div className="search-product-list">
+              <ProductCard
+                key={756756}
+                productId={5667546756}
+                name="name"
+                price={456}
+                stock={34}
+                handler={addToCartHandler}
+                photo="gfhfghfghfgh"
+              />
+
+          </div>
+        )}
+
+      </main>
+    </div>
+  );
+};
+
+export default Search;
