f6681820aa3140ed1cb9c0c79a0e8cdd42925327
Shitcoder731 <shitanshu.731@gmail.com>
2024-02-11 12:34:20 +0530

    Integrated Logout button to Login Page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 import { FaSearch,FaShoppingBag,FaSign,FaUser,FaSignOutAlt } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 const user = {_id : "asdad" , role : "admin"}
 const Header = () => {
+  const navigate = useNavigate();
   const [isOpen,setIsOpen] = useState(false);
-  const logoutHandler = () => {setIsOpen(false)}
+  const logoutHandler = () => {
+    setIsOpen(false)
+    navigate("/login")
+  }
   return (
     <nav className="header">
       <Link onClick = {() => setIsOpen(false)}  to = {"/"}>Home</Link>
