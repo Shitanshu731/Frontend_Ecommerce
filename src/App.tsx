2ef32afa1960ad28568b1ba0e1bbdbe8c922ecfc
Shitcoder731 <shitanshu.731@gmail.com>
2024-03-02 19:53:44 +0530

    Add GoogleAuthFunctionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import {lazy} from "react"
 import Loader from './components/Loader'
 import Header from './components/Header'
+import {Toaster} from "react-hot-toast"
 
 const Home = lazy(() => import("./pages/Home"))
 const Search = lazy(() => import("./pages/Search"))
@@ -79,6 +80,7 @@ const App = () => {
 
       </Routes>
       </Suspense>
+      <Toaster position ="bottom-center"/>
     </Router>
   )
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,23 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useState } from "react";
+import toast from "react-hot-toast";
 import { FcGoogle } from "react-icons/fc";
+import { auth } from "../firebase.ts";
 
 const Login = () => {
   const [gender, setGender] = useState("");
   const [date, setDate] = useState("");
 
-  const loginHandler = async () => {};
+  const loginHandler = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const { user } = await signInWithPopup(auth, provider);
+      console.log(user);
+    } catch (error) {
+      console.error("Sign In Failed:", error); // Log the error for debugging
+      toast.error("Sign In Failed"); // Display an error toast to the user
+    }
+  };
 
   return (
     <div className="login">
