74de49bd505d9640069adaa34e83130d2ef6fd95
Shitcoder731 <shitanshu.731@gmail.com>
2024-03-04 21:05:41 +0530

    Add loading and Error check to Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import ProductCard from "../components/Product-card";
 import { useLatestProductQuery } from "../redux/api/productApi";
+import toast from "react-hot-toast";
+import Loader from "../components/Loader";
 
 
 const Home = () => {
 
-  const { data } = useLatestProductQuery("");
+  const { data,isLoading,isError } = useLatestProductQuery("");
   const addToCartHandler = () => {};
 
+  if(isError) toast.error("Cannot Fetch the Products")
   return (
     <div className="home">
       <section></section>
@@ -20,7 +23,7 @@ const Home = () => {
       </h1>
 
       <main>
-        {data?.product.map((i) => (
+        {isLoading ? <Loader/> : data?.product.map((i) => (
             <ProductCard
               key={i._id}
               productId={i._id}
