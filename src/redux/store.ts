3a448d9b94ff24fb764698846803b66d7230ae3c
Shitcoder731 <shitanshu.731@gmail.com>
2024-03-25 18:38:10 +0530

    Add CartReducer to the store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,13 +2,15 @@ import  {configureStore} from "@reduxjs/toolkit"
 import { userAPI } from "./api/userApi"
 import { userReducer } from "./reducer/userReducer"
 import { productAPI } from "./api/productApi"
+import { cartReducer } from "./reducer/cartReducer";
 
 export const server = "localhost:3000/uploads";
 export const store = configureStore({
     reducer : {
         [userAPI.reducerPath] : userAPI.reducer,
         [productAPI.reducerPath] : productAPI.reducer,
-        [userReducer.name] : userReducer.reducer
+        [userReducer.name] : userReducer.reducer,
+        [cartReducer.name] : cartReducer.reducer,
     },
     middleware: (mid) => [...mid(),userAPI.middleware,productAPI.middleware],
 })
