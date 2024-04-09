570270b546e3e7d0673a61824d541b52b54d059b
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-09 19:46:53 +0530

    Create CheckOutForm Component in CheckoutPage

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -2,9 +2,17 @@ import {Elements} from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe('pk_test_51P3eqBSJ2NOymvJ7ijV2SGjIpazuYGPSwDRf3qeXtc6FPPJ4YjfYRI1PWPUKQtWyms4Z0fy5DBrYcX4zDkOvGP28009g5cOI6h');
-
+const CheckOutForm = () => {
+    return (
+        <div>
+            CheckoutForm
+        </div>
+    )
+}
 const Checkout = () => {
-  return <Elements stripe={stripePromise}></Elements>
+  return <Elements stripe={stripePromise}>
+    <CheckOutForm />
+  </Elements>
 }
 
 export default Checkout
